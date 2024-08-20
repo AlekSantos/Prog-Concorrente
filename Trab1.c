f8bf56a644516d3461682fdd02b5eb9ecb7fbc56
Alekssander Santos <101150930+AlekSantos@users.noreply.github.com>
2024-08-20 16:38:19 -0300

    Update Trab1.c

--- Trab1.c
+++ Trab1.c
@@ -2,7 +2,7 @@
 /* Profa.: Silvana Rossetto */
 /* Laboratório: 1 */
 /* Codigo: "Hello World" usando threads em C e a funcao que espera as threads terminarem */
-// Alguns prints que podem ser necessarios para corecao e foram ultizados para ajudar a fazer de forma correta, foram comentados ao longo do codigo
+// Alguns prints que podem ser necessarios para correcao e foram ultizados para ajudar a fazer de forma correta, foram comentados ao longo do codigo
 
 
 #include <stdio.h>
